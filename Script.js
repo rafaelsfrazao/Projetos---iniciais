0ba5364b813d98bfb07829ef9d75ac023fcd124b
Rafael Frazao <rafael_b9@hotmail.com>
2024-06-10 19:57:32 -0300

    Adicionando  arquivo script

--- Script.js
+++ Script.js
@@ -1,2 +1,25 @@
-console.log("Ola mundo")
+function pulaLinha(){
+    document.write("<br>");
+    document.write("<br>");
+}
 
+function mostra(frase){
+    document.write(frase);
+    pulaLinha();
+}
+
+var numero = Math.round(Math.random() * 10) //função para gerar numero aleatorio.
+var tentativas =1;
+
+while (tentativas <= 3){
+    var chute = parseInt (prompt ("Qual seu chute?"));
+
+    if (chute == numero) {
+        mostra ("Parabens! Você acertou!")
+        break; //para caso seja verdadeiro.
+        
+    }else {
+        mostra ("Errado!")
+    }
+    tentativas++
+}
